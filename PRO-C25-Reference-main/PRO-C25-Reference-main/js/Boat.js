5410d7ae4020a7db9dff8d2af9d273c91f3e94b6
gurjassingh1002 <88234385+gurjassingh1002@users.noreply.github.com>
2022-05-05 18:17:22 +0530

    Add files via upload

---
+++ PRO-C25-Reference-main/PRO-C25-Reference-main/js/Boat.js
@@ -0,0 +1,37 @@
+class Boat {
+  constructor(x, y, width, height, boatPos,boatAnimation) {
+  this.animation=boatAnimation
+  this.speed=0.05
+    this.body = Bodies.rectangle(x, y, width, height);
+    this.width = width;
+    this.height = height;
+
+    this.image = loadImage("./assets/boat.png");
+    this.boatPosition = boatPos;
+    World.add(world, this.body);
+  }
+  animate(){
+    this.speed+=0.05
+  }
+  remove(index){ 
+    this.animation=bbAnimation
+    this.speed=0.05
+    this.width=300
+    this.height=300
+    setTimeout(()=>{
+   Matter.World.remove(world, boats[index].body) 
+   delete boats[index] 
+  }, 500) 
+}
+  display() {
+    var angle = this.body.angle;
+    var pos = this.body.position;
+var index =floor(this.speed%this.animation.lenght)
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.animation[index], 0, this.boatPosition, this.width, this.height);
+    pop();
+  }
+}
